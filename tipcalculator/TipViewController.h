5a3db8557d8fbc4a792740ef812049cdfdb75654
Ryan Rawson <ryanobjc@gmail.com>
2013-09-19 00:14:51 -0700

    first commit/tip calculator walkthru

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by ryan on 9/18/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
